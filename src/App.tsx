239c3f3e693c16a1e7775b805ac6c14f90f74e0e
zhangchao <529743595@qq.com>
2020-08-11 14:57:25 +0800

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const aa = 11111;
+  const aa = {
+    a: 1,
+    b: 222,
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -11,12 +14,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
