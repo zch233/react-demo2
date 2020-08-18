817a7ec02105b95a1bc1db003f07ad1d05a996fe
zhangchao <529743595@qq.com>
2020-08-18 16:18:23 +0800

    Add WebSizeDes

---
+++ src/pages/Home/WebSiteDes.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Button } from 'antd';
+import AliIcon from '../../components/AliIcon';
+
+const Wrapper = styled.section`
+  display: flex;
+  margin-top: 60px;
+  align-items: center;
+  justify-content: space-between;
+  .des {
+    font-size: 18px;
+    h2 {
+      font-weight: 600;
+      font-size: 36px;
+      margin-top: -10px;
+    }
+  }
+`;
+const AdvanceList = styled.article`
+  display: flex;
+`;
+const AdvanceListItem = styled.div`
+  text-align: center;
+  svg {
+    font-size: 50px;
+  }
+  .advance-title {
+    color: #333;
+    margin-top: 20px;
+    font-size: 16px;
+    margin-bottom: 6px;
+    width: 13em;
+  }
+`;
+const advanceList = [
+  {
+    icon: 'advance1',
+    title: '服务',
+    des: '专注服务于知识产权领域',
+  },
+  {
+    icon: 'advance2',
+    title: '售后',
+    des: '7×8小时售后支持',
+  },
+  {
+    icon: 'advance3',
+    title: '专心',
+    des: '一对一专属客户经理',
+  },
+  {
+    icon: 'advance4',
+    title: '真实',
+    des: '值得信赖的交易平台',
+  },
+];
+const WebSiteDes: React.FC = () => {
+  return (
+    <Wrapper className={'pageWidthWithCenter'}>
+      <div className={'des'}>
+        <p>为什么选择</p>
+        <h2>第九区知产？</h2>
+        <Button>了解更多</Button>
+      </div>
+      <AdvanceList>
+        {advanceList.map((advance) => (
+          <AdvanceListItem>
+            <AliIcon icon={advance.icon} />
+            <p className={'advance-title'}>{advance.title}</p>
+            <em className={'advance-des'}>{advance.des}</em>
+          </AdvanceListItem>
+        ))}
+      </AdvanceList>
+    </Wrapper>
+  );
+};
+export default WebSiteDes;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner';
 import List from './List';
 import styled from 'styled-components';
+import WebSiteDes from './WebSiteDes';
 
 const HomeWrapper = styled.section`
   background-color: #fff;
@@ -13,6 +14,7 @@ const Home: React.FC = () => {
       <List title={'最近上新'} category={'a'} />
       <List title={'最近上新'} category={'a'} />
       <List title={'最近上新'} category={'a'} />
+      <WebSiteDes />
     </HomeWrapper>
   );
 };
