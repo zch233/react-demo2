3dabb4d298718f606fcde8d8d800fe86b6bb7930
zhangchao <529743595@qq.com>
2020-08-19 18:14:50 +0800

    Add Sign Up

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Input } from 'antd';
 
+const Wrapper = styled.section`
+  background-color: #fff;
+  padding: 30px 50px;
+  border-radius: 4px;
+  width: 400px;
+`;
 const SignIn: React.FC = () => {
-  return <h1>SignInSignInSignInSignInSignInSignInSignInSignInSignInSignIn</h1>;
+  return (
+    <Wrapper>
+      <h3>新用户注册</h3>
+      <Input className={'input'} addonBefore="手机号" defaultValue="mysite" />
+      <Input className={'input'} addonBefore="密　码" defaultValue="mysite" />
+      <Input className={'input'} addonBefore="验证码" defaultValue="mysite" />
+    </Wrapper>
+  );
 };
 
 export default SignIn;
