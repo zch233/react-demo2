1c2596a2d973b47d161c6adbeda99acb738cbc26
zhangchao <529743595@qq.com>
2020-08-20 15:54:52 +0800

    Add PatentDetail

--- src/pages/PatentDetail/Patent.tsx
+++ src/pages/PatentDetail/Patent.tsx
@@ -1,7 +1,82 @@
 import React from 'react';
+import { PatentInfo, ShopInfo, Wrapper } from './PatentStyles';
+import AliIcon from '../../components/AliIcon';
+import { Button } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Patent: React.FC = () => {
-  return <h1>patent</h1>;
+  return (
+    <Wrapper className={'pageWidthWithCenter'}>
+      <PatentInfo>
+        <div className={'imageWrapper'}>
+          <img width={'100%'} src={require('../../assert/patent/a.png')} alt="" />
+        </div>
+        <div className={'info'}>
+          <h1>一种水晶安装装置</h1>
+          <div className={'itemWrapper'}>
+            <div className={'leftItem'}>
+              <p>
+                <label>专利号</label>20191213213121
+              </p>
+              <p>
+                <label>法律状态</label>20191213213121
+              </p>
+              <p>
+                <label>发明人</label>20191213213121
+              </p>
+              <p>
+                <label>VIP会员价</label>20191213213121
+              </p>
+            </div>
+            <div className={'rightItem'}>
+              <p>
+                <label>专利类型</label>发明类型
+              </p>
+              <p>
+                <label>缴费截止</label>发明类型
+              </p>
+              <p>
+                <label>销售状态</label>发明类型
+              </p>
+              <p>
+                <label>零售价</label>30000.00
+              </p>
+            </div>
+          </div>
+          <div className={'bottom'}>
+            <Link to={'/order/confirm'}>
+              <Button size={'large'} className={'buyButton'} type={'primary'} danger>
+                立即购买
+              </Button>
+            </Link>
+            <em className={'buyTips'}>此商品已全权委托平台寄卖，平台免费提供担保交易服务。</em>
+          </div>
+        </div>
+      </PatentInfo>
+      <ShopInfo>
+        <div className={'topBar'}>
+          <label>
+            <AliIcon icon={'shop'}></AliIcon>
+          </label>
+          <p>自营</p>
+        </div>
+        <div className={'bottomBar'}>
+          <p>
+            <AliIcon icon={'shopCard'}></AliIcon>实名认证
+          </p>
+          <p>
+            <AliIcon icon={'shopOne'}></AliIcon>一手认证
+          </p>
+          <p>
+            <AliIcon icon={'shopAgent'}></AliIcon>独家代理认证
+          </p>
+          <p>
+            <AliIcon icon={'shopSend'}></AliIcon>平台寄卖
+          </p>
+        </div>
+      </ShopInfo>
+    </Wrapper>
+  );
 };
 
 export default Patent;
---
+++ src/pages/PatentDetail/PatentStyles.tsx
@@ -0,0 +1,120 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.article`
+  background-color: #fff;
+  margin: 15px auto;
+  padding: 15px;
+  display: flex;
+  align-items: center;
+`;
+export const PatentInfo = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  .imageWrapper {
+    border: 1px solid #ddd;
+    padding: 4px;
+    border-radius: 4px;
+    width: 38%;
+  }
+  .info {
+    padding: 0 3%;
+    flex: 1;
+    h1 {
+      font-size: 20px;
+      font-weight: 500;
+      border-bottom: 1px dashed #bbb;
+      padding-bottom: 1.4em;
+    }
+    .itemWrapper {
+      display: flex;
+      .leftItem {
+        flex: 1;
+      }
+      p {
+        border-bottom: 1px dashed #bbb;
+        padding-bottom: 0.9em;
+        font-size: 15px;
+        color: #000;
+        label {
+          font-weight: 600;
+          color: #666;
+          width: 4.6em;
+          display: inline-block;
+          margin-right: 1.4em;
+          text-align: justify;
+          text-align-last: justify;
+        }
+      }
+    }
+    .bottom {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-top: 15px;
+      .buyButton {
+        background-color: #d9534f;
+        border-color: #d43f3a;
+        border-radius: 6px;
+        &:hover {
+          background-color: #c9302c;
+          border-color: #ac2925;
+        }
+      }
+      .buyTips {
+        color: #777;
+        font-size: 12px;
+      }
+    }
+  }
+`;
+export const ShopInfo = styled.div`
+  border: 1px solid #faebcc;
+  text-align: center;
+  width: 270px;
+  border-radius: 4px;
+  .topBar {
+    background-color: #dff0d8;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    label {
+      font-size: 70px;
+      background-color: #fff;
+      border-radius: 50%;
+      border: 1px solid #ddd;
+      padding: 14px;
+      box-sizing: content-box;
+      line-height: 0.9;
+      color: #bbb;
+      margin-top: 25px;
+    }
+    p {
+      font-size: 20px;
+      margin-top: 15px;
+      padding: 0 0.8em;
+      border-radius: 2em;
+      color: #3c763d;
+      background-color: #fff;
+      display: inline-block;
+    }
+  }
+  .bottomBar {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: #3c763d;
+    font-size: 16px;
+    padding: 15px 0;
+    svg {
+      margin-right: 0.4em;
+      font-size: 19px;
+    }
+    p {
+      margin: 6px 0;
+      text-align: left;
+      text-indent: 5.4em;
+    }
+  }
+`;
